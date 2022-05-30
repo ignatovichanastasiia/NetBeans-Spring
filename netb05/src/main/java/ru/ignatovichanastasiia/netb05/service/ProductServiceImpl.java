5be94fefccf2ed15a09766d49042c35a09f0a10a
Tigg <ignatovichanastasiia@gmail.com>
2022-05-30 20:44:32 +0300

    Project in NetBeans(05). Spring+annotation+config. Empty template(x3)

---
+++ netb05/src/main/java/ru/ignatovichanastasiia/netb05/service/ProductServiceImpl.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ru.ignatovichanastasiia.netb05.service;
+
+/**
+ *
+ * @author ignatovichanastasiia
+ */
+public class ProductServiceImpl implements ProductService{
+    
+}
